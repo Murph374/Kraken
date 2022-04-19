345caf3b293838f4814151d3a8f1dd6dcc5b8027
garym <garym374@gmail.com>
2022-04-19 20:29:17 +0100

    first commit

--- main.py
+++ main.py
@@ -5,9 +5,21 @@ import hashlib
 import hmac
 import base64
 
-with open("keys", "r") as f:
+with open("keys.txt", "r") as f:
     lines = f.read().splitlines()
     api_key = lines[0]
     api_sec = lines[1]
 
+api_url = "https://api.kraken.com"
+
 def get_kraken_signature(urlpath, data, secret):
+    postdata = urllib.parse.urlencode(data)
+    encoded = (str(data['nonce'])+postdata).encode()
+    message = urlpath.encode() + hashlib.sha256(encoded).digest()
+    mac = hmac.new(base64.b64decode(secret), message, hashlib.sha512)
+    sigdigest = base64.b64encode(mac.digest())
+    return sigdigest.decode()
+
+def kraken_request(uri_path, data, api_key, api_sec)
+
+
